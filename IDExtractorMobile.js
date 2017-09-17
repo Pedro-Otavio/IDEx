2a867ae8f19b31364d72a1fe3880a8a7553525f9
Pedro-Otavio <Pedro-Otavio@users.noreply.github.com>
2017-09-17 04:37:05 -0300

    Add files via upload

---
+++ IDExtractorMobile.js
@@ -0,0 +1,35 @@
+let blob;
+
+start();
+
+function start() {
+    let mgbs = document.querySelectorAll('._mgb');
+    let IDs = idFromUrl(mgbs[0].querySelector('a').href);
+    for (let i = 1, len = mgbs.length; i < len; i++) {
+        IDs += '~' + idFromUrl(mgbs[i].querySelector('a').href);
+    }
+    blob = new Blob([IDs], {
+        type: "text/plain"
+    });
+    let url = window.URL.createObjectURL(blob);
+    downloadbtn(url);
+}
+
+function idFromUrl(url) {
+    let i = url.search('v=');
+    return url.substring(i + 2, url.length);
+}
+
+function downloadbtn(url) {
+    let div = document.createElement('DIV');
+    div.innerHTML = '<a id="btnDownload" href="' + url + '" download="IDs.txt" onclick="free("' + url + '")"><h1 style="color: #FFFFFF; background-color: #000000;">Download</h1></a>';
+    let content = document.getElementById('content')
+    content.insertBefore(div, content.childNodes[0]);
+    document.getElementById('btnDownload').click();
+}
+
+function free(url) {
+    window.setTimeout(function () {
+        window.URL.revokeObjectURL(url);
+    }, 500);
+}
