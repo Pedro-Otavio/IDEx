36463f444bda1029a22985fe05d32c709400e9a9
Pedro-Otavio <Pedro-Otavio@users.noreply.github.com>
2018-01-17 11:47:35 -0200

    namespacing test

--- IDExtractor.js
+++ IDExtractor.js
@@ -1,87 +1,89 @@
-var notMobile = true;
-var filename = "Playlist";
-var idFrom = d_idFrom;
-var titleFrom = d_titleFrom;
-var array = [];
+var IDExtractor = IDExtractor || (function () {
+    let notMobile = true;
+    let filename = "Playlist";
+    let idFrom = d_idFrom;
+    let titleFrom = d_titleFrom;
+    let array = [];
 
-setup();
-
-function setup() {
-    notMobile = (window.location.host != "m.youtube.com");
-    if (notMobile) {
-        array = window.ytInitialData.contents.twoColumnBrowseResultsRenderer.tabs[0].tabRenderer.content.sectionListRenderer.contents[0].itemSectionRenderer.contents[0].playlistVideoListRenderer.contents;
-        let info = window.ytInitialData.sidebar.playlistSidebarRenderer.items;
-        filename = info[1].playlistSidebarSecondaryInfoRenderer.videoOwner.videoOwnerRenderer.title.runs[0].text + " - " + info[0].playlistSidebarPrimaryInfoRenderer.title.runs[0].text;
-        start();
-    } else {
-        let container = document.querySelector('.etb .mz');
-        array = Array.from(container.querySelectorAll('.igb'));
-        filename = document.querySelector('.dab > .vjb > span').innerText;
-        idFrom = m_idFrom;
-        titleFrom = m_titleFrom;
-        start();
+    this.setup = function setup() {
+        notMobile = (window.location.host != "m.youtube.com");
+        if (notMobile) {
+            array = window.ytInitialData.contents.twoColumnBrowseResultsRenderer.tabs[0].tabRenderer.content.sectionListRenderer.contents[0].itemSectionRenderer.contents[0].playlistVideoListRenderer.contents;
+            let info = window.ytInitialData.sidebar.playlistSidebarRenderer.items;
+            filename = info[1].playlistSidebarSecondaryInfoRenderer.videoOwner.videoOwnerRenderer.title.runs[0].text + " - " + info[0].playlistSidebarPrimaryInfoRenderer.title.runs[0].text;
+        } else {
+            let container = document.querySelector('.etb .mz');
+            array = Array.from(container.querySelectorAll('.igb'));
+            filename = document.querySelector('.dab > .vjb > span').innerText;
+            idFrom = m_idFrom;
+            titleFrom = m_titleFrom;
+        }
     }
-}
 
-function start() {
-    let vidDataArr = [];
-    let vidData = {
-        id: '',
-        title: ''
-    };
-    for (let i = 0, len = array.length; i < len; i++) {
-        vidData.id = idFrom(array[i]);
-        vidData.title = titleFrom(array[i]);
-        vidDataArr.push(vidData);
-        vidData = {};
+    function start() {
+        let vidDataArr = [];
+        let vidData = {
+            id: '',
+            title: ''
+        };
+        for (let i = 0, len = array.length; i < len; i++) {
+            vidData.id = idFrom(array[i]);
+            vidData.title = titleFrom(array[i]);
+            vidDataArr.push(vidData);
+            vidData = {};
+        }
+        let PLData = {
+            playlist: vidDataArr,
+            indexArray: [],
+            index: 0,
+            time: 0
+        };
+        let data = JSON.stringify(PLData);
+        let blob = new Blob([data], {
+            type: "application/json"
+        });
+        filename += " (" + array.length + ").json";
+        download(blob);
     }
-    let PLData = {
-        playlist: vidDataArr,
-        indexArray: [],
-        index: 0,
-        time: 0
-    };
-    let data = JSON.stringify(PLData);
-    let blob = new Blob([data], {
-        type: "application/json"
-    });
-    filename += " (" + array.length + ").json";
-    download(blob);
-}
 
-function d_idFrom(e) {
-    return e.playlistVideoRenderer.videoId;
-}
-
-function d_titleFrom(e) {
-    return e.playlistVideoRenderer.title.simpleText || "<Missing title>";
-}
-
-function m_idFrom(e) {
-    let url = e.querySelector('a').href;
-    let start = url.search('v=');
-    let r = url.substring(start + 2);
-    let end = r.search('&|/');
-    if (end !== -1) {
-        return r.substring(0, end);
-    } else {
-        return r;
+    function d_idFrom(e) {
+        return e.playlistVideoRenderer.videoId;
     }
-}
 
-function m_titleFrom(e) {
-    return e.querySelector('.xn > .vjb > span').innerText;
-}
+    function d_titleFrom(e) {
+        return e.playlistVideoRenderer.title.simpleText || "<Missing title>";
+    }
 
-function download(blob) {
-    let url = window.URL.createObjectURL(blob);
-    let a = document.createElement('A');
-    a.text = "Download";
-    a.style = "color: #FFFFFF; background-color: #000000;";
-    a.download = filename;
-    a.href = url;
-    a.click();
-    window.setTimeout(function () {
-        window.URL.revokeObjectURL(url);
-    }, 500);
-}
+    function m_idFrom(e) {
+        let url = e.querySelector('a').href;
+        let start = url.search('v=');
+        let r = url.substring(start + 2);
+        let end = r.search('&|/');
+        if (end !== -1) {
+            return r.substring(0, end);
+        } else {
+            return r;
+        }
+    }
+
+    function m_titleFrom(e) {
+        return e.querySelector('.xn > .vjb > span').innerText;
+    }
+
+    function download(blob) {
+        let url = window.URL.createObjectURL(blob);
+        let a = document.createElement('A');
+        a.text = "Download";
+        a.style = "color: #FFFFFF; background-color: #000000;";
+        a.download = filename;
+        a.href = url;
+        a.click();
+        window.setTimeout(function () {
+            window.URL.revokeObjectURL(url);
+        }, 500);
+    }
+
+    return this;
+})
+IDExtractor.setup();
+IDExtractor.start();
--- Merger.js
+++ Merger.js
@@ -1,75 +1,76 @@
-let base = false;
-let dict = false;
-let fileReaderBase = new FileReader();
-let fileReaderDict = new FileReader();
-let fileBase, fileDict;
+(function () {
+    let base = false;
+    let dict = false;
+    let fileReaderBase = new FileReader();
+    let fileReaderDict = new FileReader();
+    let fileBase, fileDict;
 
-$('#fileInputBase').change(function () {
-    base = true;
-    enable();
-});
-
-$('#fileInputDictionary').change(function () {
-    dict = true;
-    enable();
-});
-$('#merge').click(merge);
-
-function enable() {
-    if (base && dict) {
-        $('#merge').prop('disabled', false);
-        base = false;
-        dict = false;
-    }
-}
-
-fileReaderBase.onerror = function (event) {
-    alert("\nFileReader error code " + event.target.error.code + "\n");
-};
-
-fileReaderDict.onerror = fileReaderBase.onerror;
-
-fileReaderBase.onload = function (event) {
-    fileBase = JSON.parse(event.target.result);
-    base = true;
-    ready();
-};
-
-fileReaderDict.onload = function (event) {
-    fileDict = JSON.parse(event.target.result);
-    dict = true;
-    ready();
-};
-
-function merge() {
-    fileReaderBase.readAsText($('#fileInputBase').prop('files')[0]);
-    fileReaderDict.readAsText($('#fileInputDictionary').prop('files')[0]);
-}
-
-
-function ready() {
-    if (!base || !dict)
-        return;
-    
-    let dictIds = [];
-    for (let i = 0, len = fileDict.playlist.length; i < len; ++i) {
-        dictIds[i] = fileDict.playlist[i].id;
-    }
-
-    for (let i = 0, len = fileBase.playlist.length; i < len; ++i) {
-        let dictIndex = dictIds.indexOf(fileBase.playlist[i].id);
-        fileBase.playlist[i].title = (dictIndex != -1) ? fileDict.playlist[dictIndex].title : fileBase.playlist[i].title;
-    }
-    let data = JSON.stringify(fileBase);
-    let blob = new Blob([data], {
-        type: 'application/json'
+    $('#fileInputBase').change(function () {
+        base = true;
+        enable();
     });
-    
-    let url = window.URL.createObjectURL(blob);
-    $('#hiddenLink').attr('href', url).attr('download', 'RenamedPlaylist.json');
-    document.querySelector('#hiddenLink').click(); //jQuery bug
-    window.setTimeout(function () {
-        window.URL.revokeObjectURL(url);
-    }, 500);
 
-}
+    $('#fileInputDictionary').change(function () {
+        dict = true;
+        enable();
+    });
+    $('#merge').click(merge);
+
+    function enable() {
+        if (base && dict) {
+            $('#merge').prop('disabled', false);
+            base = false;
+            dict = false;
+        }
+    }
+
+    fileReaderBase.onerror = function (event) {
+        alert("\nFileReader error code " + event.target.error.code + "\n");
+    };
+
+    fileReaderDict.onerror = fileReaderBase.onerror;
+
+    fileReaderBase.onload = function (event) {
+        fileBase = JSON.parse(event.target.result);
+        base = true;
+        ready();
+    };
+
+    fileReaderDict.onload = function (event) {
+        fileDict = JSON.parse(event.target.result);
+        dict = true;
+        ready();
+    };
+
+    function merge() {
+        fileReaderBase.readAsText($('#fileInputBase').prop('files')[0]);
+        fileReaderDict.readAsText($('#fileInputDictionary').prop('files')[0]);
+    }
+
+
+    function ready() {
+        if (!base || !dict)
+            return;
+
+        let dictIds = [];
+        for (let i = 0, len = fileDict.playlist.length; i < len; ++i) {
+            dictIds[i] = fileDict.playlist[i].id;
+        }
+
+        for (let i = 0, len = fileBase.playlist.length; i < len; ++i) {
+            let dictIndex = dictIds.indexOf(fileBase.playlist[i].id);
+            fileBase.playlist[i].title = (dictIndex != -1) ? fileDict.playlist[dictIndex].title : fileBase.playlist[i].title;
+        }
+        let data = JSON.stringify(fileBase);
+        let blob = new Blob([data], {
+            type: 'application/json'
+        });
+
+        let url = window.URL.createObjectURL(blob);
+        $('#hiddenLink').attr('href', url).attr('download', 'RenamedPlaylist.json');
+        document.querySelector('#hiddenLink').click(); //jQuery bug
+        window.setTimeout(function () {
+            window.URL.revokeObjectURL(url);
+        }, 500);
+    }
+})()
